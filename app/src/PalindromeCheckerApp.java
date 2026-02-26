d3a6a2b12f5c72bd7a8e3479fb622ff11e74e4e1
gy0023 <gy0023@srmist.edu.in>
2026-02-26 16:06:25 +0530

    uc5-Stack-Based Palindrome

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+import  javax.xml.namespace.QName;
+import java.util.Stack;
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String name = "noon";
+        Stack<Character> stack = new Stack<>();
+        for (char c : name.toCharArray()) {
+            stack.push(c);
+        }
+        boolean ispalindrome = true;
+        for (char c : name.toCharArray()) {
+            if (c != stack.pop()) {
+                ispalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("input text : " + name);
+        System.out.print("Is it a Palindrome? : " + ispalindrome);
+    }
+}
