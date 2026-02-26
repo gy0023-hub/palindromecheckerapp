132ceb71ed3e52196c3b006c835e02f579ccca89
gy0023 <gy0023@srmist.edu.in>
2026-02-26 15:05:08 +0530

    uc2- hardcoded palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-    static void main(String[] args) {
-        System.out.println("Welcome To The  Palindrome Checker Managment App");
-        System.out.println("Version : 1.0");
-        System.out.println("system initialized successfully.");
+    public static void main(String[] args){
+        System.out.println("input:");
+        Scanner scanner=new Scanner(System.in);
+        String name =(scanner.next());
+        name= name.toLowerCase();
+        boolean present =false;
+        for(int i =0; i<(name.length())/2;i++){
+            for(int j =(name.length())-1;j>=(name.length())/2;j--){
+                if(name.charAt(i)==name.charAt(j)){
+                    present=true;
+                    break;
+                }
+            }
+        }
+        System.out.println("is it palindrome? :"+present);
     }
 }
