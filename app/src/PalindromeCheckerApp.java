d237c9f52939193b2335d2181757415aa99ac3e9
gy0023 <gy0023@srmist.edu.in>
2026-02-26 14:13:16 +0530

    uc1-welcome message

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    static void main(String[] args) {
+        System.out.println("Welcome To The  Palindrome Checker Managment App");
+        System.out.println("Version : 1.0");
+        System.out.println("system initialized successfully.");
+    }
+}
